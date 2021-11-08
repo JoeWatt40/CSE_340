bf42f9ad6e5817ab5703bec802a3ab267e1d4698
joewatt40 <wat19030@byui.edu>
2021-11-08 13:30:05 -0800

    updating files

--- common/header.php
+++ common/header.php
@@ -1,7 +1,10 @@
- <div class="header">
-                <img src="/phpmotors/images/site/logo.png" alt="php motors logo">
-                <?php if(isset($cookieFirstname)){
-                    echo "<span>Welcome $cookieFirstname</span>";
-                    } ?>
-                <h1><a href="/phpmotors/accounts/index.php?action=login">My Account</a></h1>
-            </div>             
+<div class="header">
+    <img src="/phpmotors/images/site/logo.png" alt="php motors logo">
+    <?php if(isset($_SESSION['loggedin'])){         
+            echo "<span><a href='/phpmotors/accounts/index.php?action=vehicle'>Welcome $_SESSION[firstname]</a></span>";
+            echo "<h1><a href='/phpmotors/accounts?action=Logout'>Logout</a></h1>";
+        } else {
+            echo "<h1><a href='/phpmotors/accounts/index.php?action=login'>My Account</a></h1>";
+        }            
+      ?>     
+</div>             
